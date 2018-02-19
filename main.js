d210b2d9e098a0d4d8b102d4d5de64ce27b987e8
Kyo Yamada <hysteric.madam@gmail.com>
2018-02-20 00:24:22 +0900

    part-8

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
+Vue.component('task-list', {
+  template: `
+    <ul>
+      <task v-for="task in tasks">{{ task.task }}</task>
+    </ul>
+  `,
+  data () {
+    return {
+      tasks: [
+        { id: 0, task: 'Go to the store', complete: true },
+        { id: 3, task: 'Go to the email', complete: false },
+        { id: 1, task: 'Go to the farm', complete: true },
+        { id: 2, task: 'Go to work', complete: false }
+      ]
+    };
+  }
+});
+
 Vue.component('task', {
   template: '<li><slot></slot></li>'
 });
